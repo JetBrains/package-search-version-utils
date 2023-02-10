db7d99aeb18ea7669cde12123ed95ba37bcde6c3
Lamberto Basti <lamberto.basti@jetbrains.com>
2023-02-10 13:21:17 +0100

    Bump kotlin to 1.7.10 and gradle wrapper to 7.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "1.6.21"
+    val kotlinVersion = "1.7.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     `maven-publish`
